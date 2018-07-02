59f472be9b497c20ba02e51451d258dc8b0c0ce3
dodd0ro <yashhig@gmail.com>
2018-07-02 17:47:56 +0300

    before gulp

--- js/init.js
+++ js/init.js
@@ -1,43 +1,38 @@
-
-const THREE_DIV_ID = 'three';
-const MODEL_PATH = '../model/container3.obj'
+const THREE_DIV_ID = "three";
+const MODEL_PATH = "../model/container3.obj";
 
 const Options = {
   _highlightMode: [
-    'outline', // 0
-    'emissive' // 1
-  ], 
-  highlightMode: 'emissive',
+    "outline", // 0
+    "emissive" // 1
+  ],
+  highlightMode: "emissive",
 
   hideLabels: true,
-  _show: [
-    'container',
-    'geometry'
-  ],
-  show: 'geometry'
-
-
-}
-
+  _show: ["container", "geometry"],
+  show: "geometry"
+};
 
 /////////////////////////////////////////////////////
 
 var containerDiv, canvasWidth, canvasHeight;
 var scene, camera;
-var renderer, labelRenderer, composer, composerPasses = [];
+var renderer,
+  labelRenderer,
+  composer,
+  composerPasses = [];
 var controls, hoverer, highlighter, visibiler;
-
 
 /////////////////////////////////////////////////////
 
 function animate() {
-  requestAnimationFrame( animate );
-  
+  requestAnimationFrame(animate);
+
   // onTick
   for (let i = 0; i < animate.funcs.length; i++) {
     animate.funcs[i]();
   }
-  
+
   controls.update();
   hoverer.update();
 
@@ -45,161 +40,143 @@ function animate() {
   for (let i = 0; i < animate.funcsLast.length; i++) {
     animate.funcsLast[i]();
   }
-  if (Options.highlightMode == 'emissive') renderer.render(scene, camera)
-  labelRenderer.render( scene, camera )
-  if (Options.highlightMode == 'outline') composer.render()
-  
-};
+  if (Options.highlightMode == "emissive") renderer.render(scene, camera);
+  labelRenderer.render(scene, camera);
+  if (Options.highlightMode == "outline") composer.render();
+}
 
 animate.funcs = [];
-animate.onTick= function(func) {
+animate.onTick = function(func) {
   animate.funcs.push(func);
-}
+};
 
 animate.funcsLast = [];
 animate.onTickEnd = function(func) {
   animate.funcsLast.push(func);
-}
-
+};
 
 /////////////////////////////////////////////////////
 
 function initEvents() {
-
   /* RESIZE */
 
-  containerDiv.addEventListener( 'resize', function() {
+  containerDiv.addEventListener(
+    "resize",
+    function() {
+      canvasWidth = containerDiv.clientWidth;
+      canvasHeight = containerDiv.clientHeight;
 
-    canvasWidth = containerDiv.clientWidth;
-    canvasHeight = containerDiv.clientHeight;
+      ///
 
-    ///
+      renderer.setSize(canvasWidth, canvasHeight);
+      labelRenderer.setSize(canvasWidth, canvasHeight);
 
-    renderer.setSize( canvasWidth, canvasHeight );
-    labelRenderer.setSize( canvasWidth, canvasHeight );
+      ///
 
-    ///
+      let aspectRatio = canvasWidth / canvasHeight;
 
-    let aspectRatio = canvasWidth / canvasHeight;
-
-    for (let camera of cameras) {
-      camera.aspect = aspectRatio;
-      camera.updateProjectionMatrix();
-    }
-  
-  } , false );
+      for (let camera of cameras) {
+        camera.aspect = aspectRatio;
+        camera.updateProjectionMatrix();
+      }
+    },
+    false
+  );
 
   /* HIGHLIGHT */
 
-  let tmb_HighlightOn = new Tumblered(Options, 'highlightMode');
-  let tmb_HighlightOff = new Tumblered(Options, 'highlightMode');
+  let tmb_HighlightOn = new Tumblered(Options, "highlightMode");
+  let tmb_HighlightOff = new Tumblered(Options, "highlightMode");
 
   // outline
 
-  tmb_HighlightOn.on('outline', function(obj) {
+  tmb_HighlightOn.on("outline", function(obj) {
     highlighter.set(obj);
   });
 
-  tmb_HighlightOff.on('outline', function() {
+  tmb_HighlightOff.on("outline", function() {
     highlighter.clear();
   });
 
   // emissive
-  
-  tmb_HighlightOn.on('emissive', function(obj) {
+
+  tmb_HighlightOn.on("emissive", function(obj) {
     obj.currentHex = obj.material.emissive.getHex();
-    obj.material.emissive.setHex( new THREE.Color('#005e00').getHex () );
+    obj.material.emissive.setHex(new THREE.Color("#005e00").getHex());
   });
 
-  tmb_HighlightOff.on('emissive', function(obj) {
-    obj.material.emissive.setHex( obj.currentHex );
+  tmb_HighlightOff.on("emissive", function(obj) {
+    obj.material.emissive.setHex(obj.currentHex);
   });
 
   ///
 
   hoverer.onMouseOver(tmb_HighlightOn.getFunction());
   hoverer.onMouseOut(tmb_HighlightOff.getFunction());
-
-
 }
-
-
-
-
 
 /////////////////////////////////////////////////////
 
 function init() {
-
   /* CONTAINER DIV */
-  
-  containerDiv = document.getElementById( THREE_DIV_ID );
-  containerDiv.style.position = 'relative';
+
+  containerDiv = document.getElementById(THREE_DIV_ID);
+  containerDiv.style.position = "relative";
   if (!(containerDiv.clientWidth && containerDiv.clientHeight)) {
     throw "Container div doesn't have size";
   }
   canvasWidth = containerDiv.clientWidth;
   canvasHeight = containerDiv.clientHeight;
 
-
   /* SCENE */
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color('white')
-  scene.fog = new THREE.FogExp2( 
-    new THREE.Color( '#ffffff' ), 
-    0.0025
-  );
+  scene.background = new THREE.Color("white");
+  scene.fog = new THREE.FogExp2(new THREE.Color("#ffffff"), 0.0025);
 
   /* CAMERA */
 
-  camera = new THREE.PerspectiveCamera(
-    60,
-    canvasWidth / canvasHeight,
-    1,
-    1000
-  );
-  camera.position.set( -100, 150, 250 );
-  
+  camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 1, 1000);
+  camera.position.set(-100, 150, 250);
 
   /* RENDERER */
 
-  renderer = new THREE.WebGLRenderer( {
-    antialias: true,
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
     // alpha: true,
   });
-  renderer.setClearColor( 0xffffff );
+  renderer.setClearColor(0xffffff);
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-  renderer.setSize( canvasWidth, canvasHeight );
-  containerDiv.appendChild( renderer.domElement );
-  containerDiv.appendChild( renderer.domElement );
+  renderer.setSize(canvasWidth, canvasHeight);
+  containerDiv.appendChild(renderer.domElement);
+  containerDiv.appendChild(renderer.domElement);
 
   /* LABEL RENDERER */
 
   labelRenderer = new THREE.CSS2DRenderer();
-  labelRenderer.setSize( canvasWidth, canvasHeight );
-  labelRenderer.domElement.style.position = 'absolute';
+  labelRenderer.setSize(canvasWidth, canvasHeight);
+  labelRenderer.domElement.style.position = "absolute";
   labelRenderer.domElement.style.top = 0;
-  labelRenderer.domElement.style.overflow = 'visible'
-  containerDiv.appendChild( labelRenderer.domElement );
+  labelRenderer.domElement.style.overflow = "visible";
+  containerDiv.appendChild(labelRenderer.domElement);
   // document.querySelector('#three > div')
 
   /* CONTROLS */
 
-  controls = new THREE.OrbitControls( camera, containerDiv ); 
+  controls = new THREE.OrbitControls(camera, containerDiv);
 
-  controls.rotateSpeed  = 0.15  // 1
+  controls.rotateSpeed = 0.15; // 1
 
   controls.enableDamping = true;
-  controls.dampingFactor = 0.1  // 0.25
+  controls.dampingFactor = 0.1; // 0.25
 
   controls.minPolarAngle = Math.PI * 0.25;
   controls.maxPolarAngle = Math.PI * 0.49;
 
   controls.autoRotate = true;
-  controls.autoRotateSpeed = 0.025;  // 2
+  controls.autoRotateSpeed = 0.025; // 2
 
   controls.enableKeys = false;
   controls.enablePan = false;
@@ -207,50 +184,52 @@ function init() {
 
   /* HOVERER */
 
-  hoverer = new Hoverer ( renderer.domElement, camera );
+  hoverer = new Hoverer(renderer.domElement, camera);
 
   /* HIGHLIGHTER */
 
   highlighter = new Highlighter(
-    renderer.domElement, scene, camera, '../../assets/tri_pattern.jpg'
+    renderer.domElement,
+    scene,
+    camera,
+    "../../assets/tri_pattern.jpg"
   );
   composerPasses.push(highlighter.pass);
 
   /* VISIBILER */
 
-  visibiler  = new Visibiler(renderer.domElement, camera)
+  visibiler = new Visibiler(renderer.domElement, camera);
 
   /* COMPOSER */
 
-  composer = new THREE.EffectComposer( renderer );
+  composer = new THREE.EffectComposer(renderer);
 
-  let renderPass = new THREE.RenderPass( scene, camera );
-  composer.addPass( renderPass );
+  let renderPass = new THREE.RenderPass(scene, camera);
+  composer.addPass(renderPass);
 
   for (let pass of composerPasses) {
     composer.addPass(pass);
   }
 
-  let effectFXAA = new THREE.ShaderPass( THREE.FXAAShader );
-  effectFXAA.uniforms[ 'resolution' ].value.set( 
-    1 / canvasWidth, 1 / canvasHeight 
+  let effectFXAA = new THREE.ShaderPass(THREE.FXAAShader);
+  effectFXAA.uniforms["resolution"].value.set(
+    1 / canvasWidth,
+    1 / canvasHeight
   );
   effectFXAA.renderToScreen = true;
-  composer.addPass( effectFXAA );
-
+  composer.addPass(effectFXAA);
 }
-
 
 /////////////////////////////////////////////////////
 
 function initGui() {
-  var gui = new dat.GUI( { width: 200 } );
+  var gui = new dat.GUI({ width: 200 });
   // gui.remember(Options);
   gui.close();
 
-  gui.add(grid, 'visible').name('grid'); 
-  // gui.add(Options, 'show', Options._show).onChange (function(value){ 
-    
+  gui.add(grid, "visible").name("grid");
+  // gui.add(Options, 'show', Options._show).onChange (function(value){
+
   //   if (value == 'geometry') {
   //     scene.remove(modelGroup);
   //     scene.add(geometryGroup);
@@ -261,50 +240,46 @@ function initGui() {
 
   // });
 
-  var selectionFolder = gui.addFolder('selection');
+  var selectionFolder = gui.addFolder("selection");
   selectionFolder.open();
-  selectionFolder.add(Options, 'highlightMode', Options._highlightMode );
-  selectionFolder.add(Options, 'hideLabels').onChange (function(value){ 
+  selectionFolder.add(Options, "highlightMode", Options._highlightMode);
+  selectionFolder.add(Options, "hideLabels").onChange(function(value) {
     infoLabels.forEach(function(label) {
       if (value) {
         label.hide();
-        label.showBySelector('ul');
+        label.showBySelector("ul");
       } else {
         label.show();
-        label.hideBySelector('ul');
+        label.hideBySelector("ul");
       }
-
     });
-
   });
 
-  
-  var controlsFolder = gui.addFolder('controls');
+  var controlsFolder = gui.addFolder("controls");
   // controlsFolder.open();
-  controlsFolder.add(controls, 'enablePan');
-  controlsFolder.add(controls, 'enableZoom');
-  controlsFolder.add(controls, 'autoRotateSpeed', 0, 1);
-  controlsFolder.add(controls, 'minPolarAngle', 0, Math.PI);
-  controlsFolder.add(controls, 'maxPolarAngle', 0, Math.PI);
+  controlsFolder.add(controls, "enablePan");
+  controlsFolder.add(controls, "enableZoom");
+  controlsFolder.add(controls, "autoRotateSpeed", 0, 1);
+  controlsFolder.add(controls, "minPolarAngle", 0, Math.PI);
+  controlsFolder.add(controls, "maxPolarAngle", 0, Math.PI);
 
-  var highlighterFolder = gui.addFolder('highlighter');
+  var highlighterFolder = gui.addFolder("highlighter");
   // highlighterFolder.open();
-  highlighterFolder.add(highlighter.pass, 'edgeStrength', 0.01, 10 );
-  highlighterFolder.add(highlighter.pass, 'edgeGlow', 0.0, 1.0 , 0.1);
-  highlighterFolder.add(highlighter.pass, 'edgeThickness', 1, 4 );
-  highlighterFolder.add(highlighter.pass, 'pulsePeriod', 0.0, 5 );
-  highlighterFolder.add(highlighter.pass, 'usePatternTexture');
+  highlighterFolder.add(highlighter.pass, "edgeStrength", 0.01, 10);
+  highlighterFolder.add(highlighter.pass, "edgeGlow", 0.0, 1.0, 0.1);
+  highlighterFolder.add(highlighter.pass, "edgeThickness", 1, 4);
+  highlighterFolder.add(highlighter.pass, "pulsePeriod", 0.0, 5);
+  highlighterFolder.add(highlighter.pass, "usePatternTexture");
   var highlighterColors = {
     visibleEdgeColor: highlighter.pass.visibleEdgeColor.getHex(),
     hiddenEdgeColor: highlighter.pass.hiddenEdgeColor.getHex()
   };
-  highlighterFolder.addColor(highlighterColors, 'visibleEdgeColor').onChange( 
-    ( value ) => highlighter.pass.visibleEdgeColor.set( value )
-  );
-  highlighterFolder.addColor(highlighterColors, 'hiddenEdgeColor').onChange(
-    ( value ) => highlighter.pass.hiddenEdgeColor.set( value )
-  );
-
+  highlighterFolder
+    .addColor(highlighterColors, "visibleEdgeColor")
+    .onChange(value => highlighter.pass.visibleEdgeColor.set(value));
+  highlighterFolder
+    .addColor(highlighterColors, "hiddenEdgeColor")
+    .onChange(value => highlighter.pass.hiddenEdgeColor.set(value));
 }
 
 /////////////////////////////////////////////////////
--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var grid, cube, cubeInfo, cube2, cube2Info;
 var modelGroup, geometryGroup;
 var infoLabels = [];
 
-window.onload = function() { 
+window.onload = function() {
   init();
   initEvents();
-  grid = addGrid()
+  grid = addGrid();
   addLight();
 
   if (Options.loadModel) {
@@ -16,111 +16,98 @@ window.onload = function() {
 
   // loadModels();
   // initGeometry()
-  
+
   initGui();
   animate();
-}
-
+};
 
 function initGeometry() {
-
-  
-
   cube = addCube(100, 100, 100);
   hoverer.addObject(cube);
-  cubeInfo = new InfoLabel(cube, 'cube', [0.5,1,0.5], hoverer);
+  cubeInfo = new InfoLabel(cube, "cube", [0.5, 1, 0.5], hoverer);
   infoLabels.push(cubeInfo);
 
   cube2 = addCube(98, 23, 100);
   cube2.position.x += 60;
   cube2.position.z += 38;
   hoverer.addObject(cube2);
-  cube2Info = new InfoLabel(cube2, 'cube2', [0.6,1,0.6], hoverer);
+  cube2Info = new InfoLabel(cube2, "cube2", [0.6, 1, 0.6], hoverer);
   infoLabels.push(cube2Info);
 
   addPlain();
-  
+
   ///
+
   visibiler.addObjects([cube, cube2]);
 
   ///
 
   geometryGroup = new THREE.Group();
-  geometryGroup.add(cube, cube2)
+  geometryGroup.add(cube, cube2);
   geometryGroup.visible = true;
   scene.add(geometryGroup);
 }
 
-
-
 function addGrid() {
-  let grid = new THREE.GridHelper( 400, 40);
+  let grid = new THREE.GridHelper(400, 40);
   grid.visible = false;
-  scene.add( grid );
+  scene.add(grid);
   return grid;
 }
 
-
-function addCube(xs,ys,zs) {
-
-  var geometry = new THREE.BoxBufferGeometry( xs, ys, zs );
-  var material = new THREE.MeshPhongMaterial( { 
-    color: 0xffffff, 
-    // flatShading: true 
-  } );
-  var mesh = new THREE.Mesh( geometry, material );
-  mesh.position.y = ys/2;
+function addCube(xs, ys, zs) {
+  var geometry = new THREE.BoxBufferGeometry(xs, ys, zs);
+  var material = new THREE.MeshPhongMaterial({
+    color: 0xffffff
+    // flatShading: true
+  });
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = ys / 2;
   mesh.castShadow = true;
   mesh.receiveShadow = true;
 
   return mesh;
 }
 
-
 function addPlain() {
-
-  var geometry = new THREE.PlaneBufferGeometry( 2000, 2000, 1 );
+  var geometry = new THREE.PlaneBufferGeometry(2000, 2000, 1);
   var material = new THREE.ShadowMaterial();
   material.opacity = 0.9;
-  var mesh = new THREE.Mesh( geometry, material) ;
-  mesh.rotateX( - Math.PI / 2 );
+  var mesh = new THREE.Mesh(geometry, material);
+  mesh.rotateX(-Math.PI / 2);
 
   mesh.receiveShadow = true;
 
-  scene.add( mesh );
+  scene.add(mesh);
 
   return mesh;
-
 }
 
 function addLight() {
-
-  var dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-  dirLight.position.set( 1*200, 1.5*200, 2*200 );
+  var dirLight = new THREE.DirectionalLight(0xffffff, 1);
+  dirLight.position.set(1 * 200, 1.5 * 200, 2 * 200);
 
   dirLight.castShadow = true;
-  dirLight.shadow.mapSize.width = 512*2;  // default
-  dirLight.shadow.mapSize.height = 512*2; // default
-  
-  dirLight.shadow.camera.near = 200;       // default
-  dirLight.shadow.camera.far = 1000      // default
+  dirLight.shadow.mapSize.width = 512 * 2; // default
+  dirLight.shadow.mapSize.height = 512 * 2; // default
+
+  dirLight.shadow.camera.near = 200; // default
+  dirLight.shadow.camera.far = 1000; // default
 
   let shadowCameraSize = 300;
   dirLight.shadow.camera.left = -shadowCameraSize / 2;
   dirLight.shadow.camera.right = shadowCameraSize / 2;
   dirLight.shadow.camera.bottom = -shadowCameraSize / 2;
   dirLight.shadow.camera.top = shadowCameraSize / 2;
-  
-  scene.add( dirLight );
+
+  scene.add(dirLight);
   // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
 
   ///
 
-  var ambLight = new THREE.AmbientLight( 0x222222 );
-  scene.add( ambLight );
-
+  var ambLight = new THREE.AmbientLight(0x222222);
+  scene.add(ambLight);
 }
-
 
 ////////////
 
@@ -135,65 +122,55 @@ function loadModels() {
     // resource URL
     MODEL_PATH,
     // called when resource is loaded
-    function ( object ) {
+    function(object) {
       // console.log(object)
       // scene.add( object );
       object.visible = true;
       modelGroup = object;
       // if (object[0] == object) console.log(111)
-      object.traverse( function(child) {
+      object.traverse(function(child) {
         if (!(child instanceof THREE.Mesh)) return;
         // console.log(child)
         hoverer.addObject(child);
         objects.push(child);
         child.scale.set(0.4, 0.4, 0.4);
-        child.rotateY(Math.PI *1.47)
+        child.rotateY(Math.PI * 1.47);
         child.receiveShadow = true;
         child.castShadow = true;
-        child.material = new THREE.MeshStandardMaterial( { 
-          color: 0xffffff, 
-          // flatShading: true 
-        } );
-        
-    });
-    
-    for (let obj of objects) {
-      new InfoLabel(obj, 'container', [0.5,1,0.5], hoverer);
-    }
-      
+        child.material = new THREE.MeshStandardMaterial({
+          color: 0xffffff
+          // flatShading: true
+        });
+      });
 
+      for (let obj of objects) {
+        new InfoLabel(obj, "container", [0.5, 1, 0.5], hoverer);
+      }
     },
     // called when loading is in progresses
-    function ( xhr ) {
-      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    function(xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
     },
     // called when loading has errors
-    function ( error ) {
-      console.log( 'An error happened' );
+    function(error) {
+      console.log("An error happened");
     }
   );
-  
-
-
-
-
 }
-
-
 
 class DivRect {
   constructor(w, h) {
-    this.div = document.createElement('div');
-    this.div.className = 'testDiv';
-    this.div.style.height = w + 'px';
-    this.div.style.width = h + 'px';
-    this.div.style.background = 'red';
-    this.div.style.position = 'absolute';
+    this.div = document.createElement("div");
+    this.div.className = "testDiv";
+    this.div.style.height = w + "px";
+    this.div.style.width = h + "px";
+    this.div.style.background = "red";
+    this.div.style.position = "absolute";
     document.body.appendChild(this.div);
   }
 
-  place(x,y) {
-    this.div.style.top = x + 'px';
-    this.div.style.left = y + 'px';
+  place(x, y) {
+    this.div.style.top = x + "px";
+    this.div.style.left = y + "px";
   }
 }
